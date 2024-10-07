d7b5fa1c99fbac3cd5799571288bb9a8ad9d8bb1
PreDesign02 <predesigns02@gmail.com>
2024-10-07 18:58:51 +0530

    Fix typo: Charfield to CharField

--- music/models.py
+++ music/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Artist(models.Model):
-    name= models.Charfield(max_length=100)
+    name= models.CharField(max_length=100)
     
 class Album(models.Model):
-    title= models.Charfield(max_length=100)
+    title= models.CharField(max_length=100)
     artist= models.ForeignKey(Artist, related_name='albums', on_delete=models.CASCADE)
 
 class Song(models.Model):
-    title= models.Charfield(max_length=100)
+    title= models.CharField(max_length=100)
     album= models.ForeignKey(Album, related_name='songs', on_delete=models.CASCADE)
     
