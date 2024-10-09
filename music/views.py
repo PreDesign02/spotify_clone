6dc6573e0f0014ca05daf55a07b427d945b0305d
PreDesign02 <predesigns02@gmail.com>
2024-10-09 12:23:12 +0530

    Fixed ModelViewSet import and home view

--- music/views.py
+++ music/views.py
@@ -1,20 +1,19 @@
-# Create your views here.
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet  # Correct import
 from .models import Artist, Album, Song
 from .serializers import ArtistSerializer, AlbumSerializer, SongSerializer
 
 def home(request):
-    return render(request, 'frontend.html')
+    return render(request, 'frontend.html')  # Ensure this file exists
 
-class ArtistViewSet(viewsets.ModelViewSet):
+class ArtistViewSet(ModelViewSet):
     queryset = Artist.objects.all()
-    serializer_class= ArtistSerializer
+    serializer_class = ArtistSerializer
     
-class AlbumViewSet(viewsets.ModelViewSet):
+class AlbumViewSet(ModelViewSet):
     queryset = Album.objects.all()
-    serializer_class= AlbumSerializer
+    serializer_class = AlbumSerializer
 
-class SongViewSet(viewsets.ModelViewSet):
+class SongViewSet(ModelViewSet):
     queryset = Song.objects.all()
-    serializer_class= SongSerializer
+    serializer_class = SongSerializer
